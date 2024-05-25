ca918a368b8374909d41b4c3810247beeeaedd2c
raeland <rsvarady@gmail.com>
2024-05-25 17:49:43 -0400

    setup items

--- server.js
+++ server.js
@@ -1,32 +1,25 @@
-const express = require('express');
+const express = require('express')
 const cors = require('cors');
-const app = express();
+const app = express()
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+//const swaggerUi = require('swagger-ui-express');
+//const swaggerDocument = require('./swagger.json');
+
+// app
+//   .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+//   .use(cors())
+//   .use(express.json())
+//   .use(express.urlencoded({ extended: true }))
+//   .use('/', require('./routes'));
 
 app
-  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-  .use(cors())
-  .use(express.json())
-  .use(express.urlencoded({ extended: true }))
-  .use('/', require('./routes'));
+  .get('/', (req, res) => {
+    res.send('howdy')})
 
-const db = require('./models');
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('Connected to the database!');
-  })
-  .catch((err) => {
-    console.log('Cannot connect to the database!', err);
-    process.exit();
-  });
+//const db = require('./models');
+                                                                                                                                                                                                                                                                                                                                                  
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+  console.log(`Server is running on port ${PORT}.`)
+})
